4fa8694d4d41f40292c438fff89c6882320c9d26
Fedor24 <kurakin.fed@mail.ru>
2025-05-19 22:44:08 +0300

    убрал лишний файл из общего репозитория

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin:'http://localhost:5000'// - для фронтенда
+  origin:'https://office-tree-frontend.cloudpub.ru'// - для фронтенда
 }))
 
 app.set('views', path.join(__dirname, 'views'));
